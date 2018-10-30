4322a2fde52575b3d38a34d71a40283e0368204f
jayce <jaycejiang@qq.com>
2018-10-30 11:41:46 +0800

    Modify install script, add neovim config

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,16 @@
 if [ ! -d ~/bin/ ]; then
     mkdir -p ~/bin/
 fi
-cp ./* ~/bin/
+
+files=`ls | grep -v install`
+for file in $files
+do
+    cp $file ~/bin/
+done
+
+if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
+    echo "User bin path is correctly set"
+else
+    echo "Mssing "$HOME/bin" in path, need to add it."
+fi
+
--- vim/install.sh
+++ vim/install.sh
@@ -1,51 +1,71 @@
 #!/bin/sh
 
 if [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v git)" ]; then
-  echo 'Error: curl or git is not installed.'
-  exit
+    echo 'Error: curl or git is not installed.'
+    exit
 fi
 
-# create directory
-if [ -d $HOME/editor ]; then
-    editorpath="$HOME/editor"
-elif [ -d $HOME/Editor ]; then
-    editorpath="$HOME/Editor"
-else
-    editorpath="$HOME/editor"
-    mkdir -p $editorpath
-    mkdir -p $editorpath/vim
-fi
-
-echo "editor path: $editorpath"
+currentpath=`pwd`
+echo "Current path: $currentpath"
 
 # backup old vimrc files
-if [ -f $HOME/.vimrc ]; then
-    mv $HOME/.vimrc $editorpath/vimrc.old
-    echo "back up old vimrc file to $editorpath/vimrc.old"
+if [ -f $HOME/.vimrc ] && [ ! `readlink $HOME/.vimrc` = $currentpath/vimrc ]; then
+    mv $HOME/.vimrc $currentpath/vimrc.old
+    echo "Back up old vimrc file to $currentpath/vimrc.old"
 fi
-if [ -d $HOME/.vim ]; then
-    mv $HOME/.vim $editorpath/vim.old
-    echo "back up old vim directory to $editorpath/vim.old"
-fi
-
-# overwite with new vimrc file
-cp ./vimrc $editorpath/vimrc
-ln -s $editorpath/vimrc $HOME/.vimrc
-ln -s $editorpath/vim $HOME/.vim
-
-# install plugin Vundle if not installed
-if [ ! -d $editorpath/vim/bundle ]; then
-    mkdir -p $editorpath/vim/bundle
-fi
-if [ ! -d $editorpath/vim/bundle/Vundle.vim ]; then
-    mkdir -p $editorpath/vim/bundle/Vundle.vim
-    git clone https://github.com/VundleVim/Vundle.vim.git $editorpath/vim/bundle/Vundle.vim
+if [ -d $HOME/.vim ] && [ ! `readlink $HOME/.vim` = $currentpath/vimdir ]; then
+    mv $HOME/.vim $currentpath/vim.old
+    echo "Back up old vim directory to $currentpath/vim.old"
 fi
 
-# install flake8 for syntax checking
-echo "Please install flake8 for syntax checking"
-# pip install flake8
+# link to new file
+if [ ! -d $currentpath/vimdir ]; then
+    mkdir -p $currentpath/vimdir
+fi
+ln -sfT $currentpath/vimdir $HOME/.vim
+ln -sfT $currentpath/vimrc  $HOME/.vimrc
+echo "Link $HOME/.vim   to $currentpath/vimdir"
+echo "Link $HOME/.vimrc to $currentpath/vimrc"
 
-if [ -d fasdfsa ]; then
+# update neovim config
+read -p "Update neovim config? (y/n, default: y)" yn
+if [ -z "$yn" ]; then
+    update_nvim=yes
+else
+    case $yn in
+        [Yy]* ) echo "aha?"; update_nvim=yes;;
+    esac
+fi
+if [ "$update_nvim" = "yes" ]; then
+    if [ ! -d $HOME/.config/nvim ]; then
+        mkdir -p $HOME/.config/nvim
+    fi
+    if [ -f $HOME/.config/nvim/init.vim ]; then
+        mv $HOME/.config/nvim/init.vim ./init.vim.old
+        echo "Backup old $HOME/.config/nvim/init.vim to ./init.vim.old"
+    fi
+    ln -s $currentpath/vimrc $HOME/.config/nvim/init.vim
+    echo "Link $HOME/.config/nvim/init.vim to $currentpath/vimrc"
+fi
 
+# for Vim install plugin Vundle if not installed
+if [ ! -d $currentpath/vimdir/bundle/Vundle.vim ]; then
+    mkdir -p $currentpath/vimdir/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $currentpath/vim/bundle/Vundle.vim
+    echo "For Vim, installed Vundle.vim to $currentpath/vimdir/bundle/Vundle.vim"
+fi
+# for Neovim, install vim-plug if not installed
+if [ -x "$(command -v nvim)" ]; then
+    if [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
+        curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        echo "For neovim, installed vim-plug to $HOME/.local/share/nvim/site/autoload/plug.vim"
+    fi
+fi
+
+# install suggestions
+# echo "Suggest to install flake8 for syntax checking:"
+# echo "--> pip install flake8"
+# echo "Suggest to install exuberant-ctags:"
+# echo "--> sudo apt-get install exuberant-ctags"
 
